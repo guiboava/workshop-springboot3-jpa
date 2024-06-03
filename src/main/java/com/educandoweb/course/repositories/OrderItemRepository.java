dc1bd2dedf64744955d15e86691351b88a6f5c85
Guilherme da Silva Boava <guiboava@hotmail.com>
2024-06-03 10:40:18 -0300

    OrderItem, many-to-many association with extra attributes - CONTINUATION

---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.OrderItem;
+
+import jakarta.persistence.Id;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Id>{
+
+}
